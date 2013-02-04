97885edece6be0ce9e685d5cff4885de47d2d815
Guillaume Miranda <guillaumemiranda@msn.com>
2013-02-04 22:56:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetTutoreMissions::Application.config.secret_token = '92e6a2eda21cae2802c9fda3153ff28bafdd4d23f968921ec4d14178e252aef1cbfb8ce9d7ab4dd19b3ac86921c2876eee729b9d54123b68d66a81613aaa3b6d'
